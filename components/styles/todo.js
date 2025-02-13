0057831e30ef0e9de4cc2a3cd43a5fbd3774a60e
sankitdev <ankitsingh635101@gmail.com>
2025-02-13 09:41:27 +0530

    feat: Add reusable styles for Todo component layout

---
+++ components/styles/todo.js
@@ -0,0 +1,5 @@
+export const styles = {
+  container: "min-h-screen flex items-center justify-center p-4",
+  taskRow: "flex justify-between gap-2 my-2",
+  actionGroup: "flex items-center justify-center gap-2",
+};
