146f469578b0b53086609a9ae2bf66a66378c377
sankitdev <ankitsingh635101@gmail.com>
2025-02-13 09:41:43 +0530

    feat: Refactor Todo component and extract TodoItem for better code organization

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -8,22 +8,16 @@ import {
 } from "@/components/ui/card";
 import { Input } from "./ui/input";
 import { useState } from "react";
-import {
-  addTodo,
-  deleteTodo,
-  editTodo,
-  toggleTodo,
-} from "@/actions/todoAction";
+import { addTodo } from "@/actions/todoAction";
 import { todoType } from "@/types/todoType";
 import { Button } from "./ui/button";
-import { Checkbox } from "./ui/checkbox";
-import { Label } from "./ui/label";
-import { Pencil, Trash2 } from "lucide-react";
+import TodoItem from "./TodoItem";
+import { styles } from "@/components/styles/todo";
 interface Todo {
   tasks: todoType[];
 }
 export default function Todo({ tasks }: Todo) {
-  const [task, setTask] = useState("testing");
+  const [task, setTask] = useState("");
   const [editId, setEditId] = useState<string | null>(null);
   const [editText, setEditText] = useState("");
   const handleAdd = () => {
@@ -34,16 +28,9 @@ export default function Todo({ tasks }: Todo) {
       console.error("Failed", error);
     }
   };
-  const handleDelete = (id: string) => {
-    if (!id) return;
-    try {
-      deleteTodo(id);
-    } catch (error) {
-      console.error("Failed", error);
-    }
-  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className={styles.container}>
       <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle>Todo App</CardTitle>
@@ -60,59 +47,14 @@ export default function Todo({ tasks }: Todo) {
             <Button onClick={handleAdd}>Add</Button>
           </div>
           {tasks.map((todo) => (
-            <div key={todo.id} className="flex justify-between gap-2 my-2">
-              <div className="flex items-center justify-center gap-2">
-                <Checkbox
-                  id={todo.id}
-                  defaultChecked={todo.isComplete}
-                  onCheckedChange={() => toggleTodo(todo.id)}
-                />
-                {editId !== todo.id ? (
-                  <Label htmlFor={todo.id} className="text-lg">
-                    {todo.task}
-                  </Label>
-                ) : (
-                  <Input
-                    value={editText}
-                    onChange={(e) => setEditText(e.target.value)}
-                  />
-                )}
-              </div>
-              <div className="flex items-center justify-center gap-2">
-                {editId !== todo.id ? (
-                  <Pencil
-                    onClick={() => {
-                      setEditText(todo.task);
-                      setEditId(todo.id);
-                    }}
-                  />
-                ) : (
-                  <Button
-                    className="bg-blue-400"
-                    onClick={() => {
-                      editTodo(editId, editText);
-                      setEditId(null);
-                    }}
-                  >
-                    Save
-                  </Button>
-                )}
-
-                {editId !== todo.id ? (
-                  <Trash2
-                    onClick={() => handleDelete(todo.id)}
-                    className="text-red-500"
-                  />
-                ) : (
-                  <Button
-                    className="bg-red-500"
-                    onClick={() => setEditId(null)}
-                  >
-                    Cancel
-                  </Button>
-                )}
-              </div>
-            </div>
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              editId={editId}
+              setEditId={setEditId}
+              setEditText={setEditText}
+              editText={editText}
+            />
           ))}
         </CardContent>
       </Card>
---
+++ components/TodoItem.tsx
@@ -0,0 +1,78 @@
+import { Pencil, Trash2 } from "lucide-react";
+import { Checkbox } from "./ui/checkbox";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { deleteTodo, editTodo, toggleTodo } from "@/actions/todoAction";
+import { styles } from "@/components/styles/todo";
+export default function TodoItem({
+  todo,
+  editId,
+  setEditText,
+  setEditId,
+  editText,
+}) {
+  const handleDelete = (id: string) => {
+    if (!id) return;
+    try {
+      deleteTodo(id);
+    } catch (error) {
+      console.error("Failed", error);
+    }
+  };
+  return (
+    <div className={styles.taskRow}>
+      <div className={styles.actionGroup}>
+        <Checkbox
+          id={todo.id}
+          defaultChecked={todo.isComplete}
+          onCheckedChange={() => toggleTodo(todo.id)}
+        />
+        {editId !== todo.id ? (
+          <Label htmlFor={todo.id} className="text-lg">
+            {todo.task}
+          </Label>
+        ) : (
+          <Input
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+          />
+        )}
+      </div>
+      <div className={styles.taskRow}>
+        {editId !== todo.id ? (
+          <Pencil
+            onClick={() => {
+              setEditText(todo.task);
+              setEditId(todo.id);
+            }}
+          />
+        ) : (
+          <Button
+            className="bg-blue-400 hover:bg-blue-600"
+            onClick={() => {
+              editTodo(editId, editText);
+              setEditId(null);
+            }}
+          >
+            Save
+          </Button>
+        )}
+
+        {editId !== todo.id ? (
+          <Trash2
+            onClick={() => handleDelete(todo.id)}
+            className="text-red-500 hover:text-red-600"
+          />
+        ) : (
+          <Button
+            className="bg-red-500 hover:text-red-600"
+            onClick={() => setEditId(null)}
+          >
+            Cancel
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
