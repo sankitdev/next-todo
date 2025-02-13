b565b73bc05cc48e3937a3ad72e7339d9849c749
sankitdev <ankitsingh635101@gmail.com>
2025-02-13 11:41:18 +0530

    feat: Wrap Todo component with ParticlesBackground for enhanced visual presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { getTodo } from "@/actions/todoAction";
+import ParticlesBackground from "@/components/kokonutui/particles-background";
 import Todo from "@/components/Todo";
 
 export default async function Home() {
   const todo = await getTodo();
-  return <Todo tasks={todo} />;
+  return (
+    <ParticlesBackground>
+      <Todo tasks={todo} />
+    </ParticlesBackground>
+  );
 }
