9773c684096ffae2833dfb0f068f842697e6f2d8
sankitdev <ankitsingh635101@gmail.com>
2025-02-13 12:18:57 +0530

    feat: Update Todo component container width for improved layout responsiveness

--- components/styles/todo.js
+++ components/styles/todo.js
@@ -1,5 +1,5 @@
 export const styles = {
-  container: "min-h-screen flex items-center justify-center p-4",
+  container: "w-1/2 lg:w-96 flex items-center justify-center p-4",
   taskRow: "flex justify-between gap-2 my-2",
   actionGroup: "flex items-center justify-center gap-2",
 };
